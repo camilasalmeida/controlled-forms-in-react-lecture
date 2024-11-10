b385f2ab84f37aedd28f602d1cb5ffcaede81c05
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-09 20:17:55 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -49,24 +49,3 @@ const App = () => {
 
 export default App;
 
-/*
-- Make a simple input
-1. Add state
-2. Add an input value tied to state , value={cityInput};
-3. Add a change handler function
-
-You’ll always need some kind of state, some kind of user input element, and a handler function that sets state inside it.
-
-- cityInput(the state), is controlling the value of the input field. Every time the state changes, the input field updates to show the new value.
-*/
-
-
-  // const handleFirstNameChange = (event) => {
-  //   setFirstName(event.target.value);
-  //   console.log(firstName)
-  // };
-
-  // const handleLastNameChange = (event) => {
-  //   setLastName(event.target.value);
-  //   console.log(lastName)
-  // }
